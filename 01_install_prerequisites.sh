f18c4aa441b899be5df5ad98a67834851fdf0133
Marc R. Hoffmann <hoffmann@mountainminds.com>
2024-01-16 10:29:08 +0000

    Finalize Android setup

--- 01_install_prerequisites.sh
+++ 01_install_prerequisites.sh
@@ -1,5 +1,7 @@
 mkdir -p work/android-sdk
 
-# Find latest dwonload URL here ("Command line tools only"): https://developer.android.com/studio
+# Find latest download URL here ("Command line tools only"): https://developer.android.com/studio
 wget -P work https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
 sudo unzip -d work/android-sdk work/commandlinetools-linux-*_latest.zip
+
+yes | work/android-sdk/cmdline-tools/bin/sdkmanager --sdk_root=work/android-sdk --licenses
--- 02_download_reproducer.sh
+++ 02_download_reproducer.sh
@@ -2,4 +2,4 @@ mkdir work
 wget -P work https://github.com/jacoco/jacoco/files/13935717/jacoco-unit-test-problem.zip
 unzip -d work work/jacoco-unit-test-problem.zip
 
-echo "sdk.dir=/usr/lib/android-sdk" > work/jacoco-unit-test-problem/local.properties
+echo "sdk.dir=../android-sdk" > work/jacoco-unit-test-problem/local.properties
