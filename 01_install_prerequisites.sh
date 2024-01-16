9cbe258bd6c3c7ec48b2e161f85f7e3c24c86b7f
Marc R. Hoffmann <hoffmann@mountainminds.com>
2024-01-16 10:06:16 +0000

    WIP: Try to install Android manually :(

--- 01_install_prerequisites.sh
+++ 01_install_prerequisites.sh
@@ -1,7 +1,5 @@
-sudo apt update && sudo apt install -y android-sdk
+mkdir -p work/android-sdk
 
 # Find latest dwonload URL here ("Command line tools only"): https://developer.android.com/studio
 wget -P work https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
-sudo unzip -d /usr/lib/android-sdk work/commandlinetools-linux-*_latest.zip
-
-yes | /usr/lib/android-sdk/cmdline-tools/bin/sdkmanager --sdk_root=/usr/lib/android-sdk --licenses
+sudo unzip -d work/android-sdk work/commandlinetools-linux-*_latest.zip
